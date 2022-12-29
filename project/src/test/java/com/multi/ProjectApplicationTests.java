7896c19a54f2b235ad14e3cdd9b964e9a5ba9187
WooJin <zathy1225@gmail.com>
2022-12-29 17:26:45 +0900

    죄송해요 ㅠㅜ

--- project/src/test/java/com/multi/ProjectApplicationTests.java
+++ project/src/test/java/com/multi/ProjectApplicationTests.java
@@ -6,10 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProjectApplicationTests {
 	
-	private String hh="multi";
-	private String abb="campus";
-	private String res=hh+abb;
-	
+
 	@Test
 	void contextLoads() {
 	}
